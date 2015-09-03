02ae285904d7230c87be66e8086a8e9023a195e2
LeFrosty <FrostyEA@Outlook.comgit config --global user.name LeFrostygit config --global user.email FrostyEA@Outlook.com>
2015-09-02 22:28:55 -0400

    Meta# Initial Upload

---
+++ MetaSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = SharpDX.Color;
+using Color2 = System.Drawing.Color;
+
+namespace MetaSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += OnGameLoad;
+        }
+
+        static void OnGameLoad(EventArgs args)
+        {
+            try
+            {
+                var cs = ObjectManager.Player.ChampionName;
+                var say = ("Meta# Loaded : ");
+                var def = ("Meta# Doesn't Support : ");
+                switch (cs)
+                {
+                    case "Heimerdinger":
+                        new Heimerdinger();
+                        Notifications.AddNotification(say + cs, 5000).SetTextColor(Color2.LawnGreen);
+                        break;
+
+                    default:
+                        Notifications.AddNotification(say + def, 5000).SetTextColor(Color2.Crimson);
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
